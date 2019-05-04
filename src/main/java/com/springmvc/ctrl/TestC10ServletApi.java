89f1e0d1fd853331e8e5e87693801ec9713de5cd
xiluhua <xlh505440@163.com>
2019-05-04 13:42:46 +0800

    add WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(request.getServletContext(), key);

--- src/main/java/com/springmvc/ctrl/TestC10ServletApi.java
+++ src/main/java/com/springmvc/ctrl/TestC10ServletApi.java
@@ -4,7 +4,11 @@ import com.springmvc.pojo.User;
 import com.springmvc.statics.Cons;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.FrameworkServlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +19,9 @@ public class TestC10ServletApi {
 
 	@RequestMapping(value="C10")
 	public void test(HttpServletRequest request, HttpServletResponse response, Writer writer) {
+        String key = FrameworkServlet.SERVLET_CONTEXT_PREFIX + "springmvc";
+		WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(request.getServletContext(), key);
+
 		System.out.println("C10!!!");
 		System.out.println("username: "+request.getParameter("username"));
 		System.out.println("response: "+response);
